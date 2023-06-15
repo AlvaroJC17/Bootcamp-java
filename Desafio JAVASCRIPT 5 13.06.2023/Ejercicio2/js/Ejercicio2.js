a34d3c5c50554106b05a4a4724362b3606277ec0
Alvaro <alvarocortesia@gmail.com>
2023-06-14 23:41:37 -0300

    se quitaron variables que no se usan y se agreagaron mas comentarios

--- Desafio JAVASCRIPT 5 13.06.2023/Ejercicio2/js/Ejercicio2.js
+++ Desafio JAVASCRIPT 5 13.06.2023/Ejercicio2/js/Ejercicio2.js
@@ -1,12 +1,9 @@
 
 
-const creacionDiv = document.getElementById("b1"); // se obtiene info del boton
-const contenedorDiv = document.getElementById("b2"); // se obtiene info del contendor "b2"
+const creacionDiv = document.getElementById("buttonAdd"); // se obtiene info del boton "+" para crear inputs
+const contenedorDiv = document.getElementById("contenedorInputs"); // se obtiene info del contendor "b2"
 const divBody = document.getElementById("divBody"); //contenedor donde se va imprimir los resultados
 const botonReset = document.getElementById("botonReset"); // se obtiene datos del boton para reset
-
-
-var contadorEliminador = 0;
 
 // funcion para crear los div mediante el boton + y guardarlos todos dentro de otro div contenedor
 creacionDiv.addEventListener("click", function (event) {
@@ -15,13 +12,11 @@ creacionDiv.addEventListener("click", function (event) {
 
     let divContenedor = document.createElement("div");
     divContenedor.classList.add("row", "container-fluid");
-    divContenedor.id = "eliminador" + contadorEliminador;
 
     let inputTexto = document.createElement("input");
     inputTexto.type = "text";
     inputTexto.placeholder = "Ingrese Categoria";
     inputTexto.classList.add("form-control", "mt-3", "col", "valorOpciones");
-
 
     let inputPorcentaje = document.createElement("input");
     inputPorcentaje.type = "number";
@@ -31,53 +26,50 @@ creacionDiv.addEventListener("click", function (event) {
     let botonEliminador = document.createElement("button");
     botonEliminador.textContent = "-";
     botonEliminador.classList.add("col-1", "btn", "btn-danger", "ms-2", "mt-3", "botonEliminador");
-    botonEliminador.onclick = function(){
+    botonEliminador.onclick = function () {  // funcion para que el boton "-" elimine el div que lo contiene
         contenedorDiv.removeChild(divContenedor);
     }
-    
+
     divContenedor.appendChild(inputTexto);
     divContenedor.appendChild(inputPorcentaje);
     divContenedor.appendChild(botonEliminador);
     contenedorDiv.appendChild(divContenedor);
-
-    contadorEliminador++;
 });
 
 // Funcion para obtener los nombres de las categorias, el valor del presupuesto y los valores de porcentaje
-
 const datosObtenidos = document.getElementById("miFormulario").addEventListener("submit", function (event2) {
     event2.preventDefault();
 
     let presupuesto = parseFloat(document.getElementById("input1").value);
     const valorCategoria = document.getElementsByClassName("valorOpciones");
-    let valorPorcentaje = document.getElementsByClassName("valorPorcentaje");
+    const valorPorcentaje = document.getElementsByClassName("valorPorcentaje");
 
-    let porcentaje = 0;
+    let Totalpresupuesto = 0;
+    let redondeoPorcentaje = 0;
     let validacionPorcentaje = 0;
     let contador = 0;
-    let redondeoPorcentaje = 0;
 
-    // Validacion para que el total de porcentajes no pase el 100%
+    // bucle para determinar el total de los porcentajes ingresados
     for (let y = 0; y < valorPorcentaje.length; y++) {
         validacionPorcentaje += parseFloat(valorPorcentaje[y].value);
     }
 
-    // Bucle para recoorrer los arrays de datos1 y datos2
+    // Bucle para recoorrer los arrays las categorias y los porcentajes
     for (let i = 0; i < valorCategoria.length; i++) {
 
         contador++;
-        const identificadorDiv = "resultado" + contador; // se crea id del div dinamico
+        let identificadorDiv = "resultado" + contador; // se crea id del div dinamico
 
-        if (!isNaN(presupuesto) && valorCategoria[i].value != "" && valorPorcentaje[i].value != "") {
+        if (!isNaN(Totalpresupuesto) && valorCategoria[i].value != "" && valorPorcentaje[i].value != "") { // validacion para que los campos no esten vacios
 
-            if (validacionPorcentaje > 0 && validacionPorcentaje <= 100) {
-                porcentaje = presupuesto * (parseFloat(valorPorcentaje[i].value) / 100); // se convierte el porcentaje y se calcula el presupuesto.
-                redondeoPorcentaje = porcentaje.toFixed(2);
+            if (validacionPorcentaje > 0 && validacionPorcentaje <= 100) { // validacion para que el porcentaje no sea negativo o mayor a 100%
+                Totalpresupuesto = presupuesto * (parseFloat(valorPorcentaje[i].value) / 100);
+                redondeoPorcentaje = Totalpresupuesto.toFixed(2);
 
-                // se crea objeto con todas los valores
+                // se crea objetos con los valores de las categorias, los porcentajes individuales y el valor de calculo de prespuesto para cada categoria 
                 const objetoOpciones = { Categoria: valorCategoria[i].value, Porcentaje: valorPorcentaje[i].value, Presupuesto: redondeoPorcentaje };
 
-                // se imprime en pantalla con los valores de los objetos usando la funcion de impresion "impresionPantalla"
+                // se imprime en pantalla los div con los valores de los objetos y agregandoles un id dinamico, usando la funcion "impresionPantalla" 
                 impresionPantalla(objetoOpciones.Porcentaje, objetoOpciones.Categoria, objetoOpciones.Presupuesto, identificadorDiv);
             } else {
                 alert("Los porcentajes repartidos exceden el 100%");
@@ -90,7 +82,8 @@ const datosObtenidos = document.getElementById("miFormulario").addEventListener(
     }
 })
 
-// funcion para crear div e imprimir en pantalla el resultado  
+
+// funcion para crear div con id dinamicos e imprimir en pantalla el resultado de cada de los inputs creados 
 function impresionPantalla(valorPorcentaje, categoria, calculoPresupuesto, idDiv) {
     let imprimirDiv = document.createElement("div");
     imprimirDiv.id = idDiv;
@@ -107,7 +100,10 @@ botonReset.addEventListener("click", function () {
         contenedorDiv.firstChild.remove();
     }
 })
-    
+
+
+
+
   
 
 
